704af96d767ccd952047b9402d36d898c9c16155
vrnprkh <paintingburritos@gmail.com>
2024-10-16 19:03:05 -0400

    server

--- model.py
+++ model.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import json
 from pprint import pprint
 

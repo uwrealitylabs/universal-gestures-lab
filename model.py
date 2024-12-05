805de9e3d2a17e613c3be164ac375103171d22f2
vrnprkh <paintingburritos@gmail.com>
2024-12-04 20:27:20 -0500

    Remove comment

--- model.py
+++ model.py
@@ -7,11 +7,9 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-# import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-# import matplotlib.pyplot as plt
 import json
 from pprint import pprint
 from sklearn.metrics import roc_auc_score, precision_recall_curve, auc

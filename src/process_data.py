e69e4622f1ab6c0b6118da225c5e0f3b63e77c08
Patrick Yuan <patthepatat@gmail.com>
2025-02-13 21:16:53 -0500

    edit process data script to use other gestures' positive data as negative data

--- src/process_data.py
+++ src/process_data.py
@@ -7,28 +7,35 @@ import json
 
 # WILL NEED TO SOMEHOW MODIFY THE DATA TO SUPPORT > 2 CLASSES
 
-def process_data(dataset_file):
+def process_data(dataset_file, desired_confidence=None):
     with open(dataset_file) as f:
         json_data = json.load(f)
 
     data_list = []
     for entry in json_data:
         hand_data = entry['handData']  
-        confidence = entry['confidence']  
+        confidence = entry['confidence'] if desired_confidence is None else desired_confidence
         combined_data = hand_data + [confidence]
         data_list.append(combined_data)
 
     # Convert the list of lists to a torch tensor
     return data_list
     
-def split(dire = "src/data_with_transform"):
-    data_files = os.listdir(dire)
+def split(positive_data, data_dir="src/data_with_transform"):
+    print(f"\n\n\nSplitting data for {positive_data}")
+    print("--------------------------------")
+    data_files = os.listdir(data_dir)
     
     print("Loading data files")
     full_dataset = []
     train = []
+    print("--------------------------------")
     for dataset_name in data_files:
-        full_dataset.extend(process_data(dire + "/" +dataset_name))
+        print(f"Processing {"positive" if positive_data in dataset_name else "negative"} data : {dataset_name}")
+        desired_confidence = 1 if positive_data in dataset_name else 0
+        full_dataset.extend(process_data(data_dir + "/" +dataset_name, desired_confidence=desired_confidence))
+    print(f"Total data: {len(full_dataset)}")
+    print("--------------------------------")
 
     print("Splitting data into training and testing")
     train_size =  int(0.8 * len(full_dataset))
@@ -37,14 +44,29 @@ def split(dire = "src/data_with_transform"):
 
     print("Processing training data")
     train_tensor = torch.tensor(train)
-    torch.save(train_tensor, "src/train_data/train_0.pt")
+    torch.save(train_tensor, "src/train_data/train_" + positive_data + ".pt")
     
     print("Processing testing data")
     test_tensor = torch.tensor(test)
-    torch.save(test_tensor, "src/test_data/test_0.pt")
+    torch.save(test_tensor, "src/test_data/test_" + positive_data + ".pt")
+    print("--------------------------------")
+    print(f"Finished splitting data for {positive_data}")
+    print("--------------------------------")
     
 def main():
-    split()
+    for gesture in [
+        "erm_actually",
+        "finger_gun",
+        "fist_up",
+        "paper",
+        "peace_sign",
+        "rock",
+        "scissors",
+        "thumbs_up",
+        "thumbs_down",
+        "wave",
+    ]:
+        split(positive_data=gesture, data_dir="src/data_with_transform")
     # process_data(data_path, output_path)
 
 if __name__ == "__main__":
